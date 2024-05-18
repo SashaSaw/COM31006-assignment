cdbbe4b2e2a009ec177da5d81a80e7bc81a007db
sawsa <asaw1@sheffield.ac.uk>
2024-05-18 19:37:26 +0100

    added images and some new functions

--- COM31006.py
+++ COM31006.py
@@ -4,15 +4,15 @@ import PySimpleGUI as sg
 import matplotlib.pyplot as plt
 from random import randrange
 
-def resize(img, x, y):
-    if img.shape[0] > x:
-        img = cv2.resize(img, [x, img.shape[1]])
+def resize(img, w, h):
+    if img.shape[1] > w:
+        img = cv2.resize(img, [w, img.shape[0]])
 
-    if img.shape[1] > y:
-        img = cv2.resize(img, [img.shape[0], y])
+    if img.shape[0] > h:
+        img = cv2.resize(img, [img.shape[1], h])
     return img
 
-def harris_corner(img):
+def harris_corner(img, dilated):
     # Convert cv2 Image to Grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -22,86 +22,121 @@ def harris_corner(img):
     # Use cv2 harris corner function, img = input image, 2 = block size, 3 = ksize, 0.04 =
     dst = cv2.cornerHarris(gray, 2, 3, 0.04)
 
+    if dilated:
+        # make the harris corner points larger to see them easier
+        dst = cv2.dilate(dst, None)
+
     # if dst value is above 10 percent of the max dst value for all pixels then paint that pixel red
     img[dst > 0.01 * dst.max()] = [0, 0, 255]
 
-    img = resize(img, 700, 500)
     return img
 
-def harris_corner_dilated(img):
+def sift_detector(img, flags, return_image):
     # Convert cv2 Image to Grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # convert the Grayscale image to float32
-    gray = np.float32(gray)
-
-    # Use cv2 harris corner function, img = input image, 2 = block size, 3 = ksize, 0.04 =
-    dst = cv2.cornerHarris(gray, 2, 3, 0.04)
-
-    # result is dilated for marking the corners, not important
-    dst = cv2.dilate(dst, None)
-
-    # if dst value is above 10 percent of the max dst value for all pixels then paint that pixel red
-    img[dst > 0.01 * dst.max()] = [0, 0, 255]
-
-    img = resize(img, 700, 500)
-    return img
-
-def sift_detector(img, flags):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
+    # use cv2 to create a SIFT object
     sift = cv2.SIFT_create()
+    # use sift object to detect key points in the grayscale image
     kp = sift.detect(gray, None)
 
-    if flags:
-        img = cv2.drawKeypoints(gray, kp, img, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-    else:
-        img = cv2.drawKeypoints(gray, kp, img)
+    if flags: # use key points and the image to draw them onto the image with flags
+        new_img = cv2.drawKeypoints(img, kp, img, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    else: # use key points and the image to draw them onto the image without flags
+        new_img = cv2.drawKeypoints(img, kp, img)
 
+    # use sift object to compute the descriptors
     kp, des = sift.compute(gray, kp)
+    print(des[1])
+    print(des[1].shape)
 
-    img = resize(img, 700, 500)
-    return img, kp, des
+    # return img with keypoints drawn on, set of key points and set of descriptors
+    if return_image:
+        return new_img, kp, des
+    else:
+        return kp, des
 
-def brute_force_matching(img1, img2):
-    #img1 = cv2.imread('Images/left.png', cv2.IMREAD_GRAYSCALE)  # queryImage
-    #img2 = cv2.imread('Images/right.png', cv2.IMREAD_GRAYSCALE)  # trainImage
+def calculate_ssd(descriptor_X, descriptor_Y):
+    descriptor_X = np.array(descriptor_X)
+    descriptor_Y = np.array(descriptor_Y)
 
-    # Initiate SIFT detector
-    sift = cv2.SIFT_create()
+    squared_diff = (descriptor_X - descriptor_Y) ** 2
 
-    # find the keypoints and descriptors with SIFT
-    kp1, des1 = sift.detectAndCompute(img1, None)
-    kp2, des2 = sift.detectAndCompute(img2, None)
+    sum_of_squared_diff = np.sum(squared_diff)
 
-    # BFMatcher with default params
-    bf = cv2.BFMatcher()
-    matches = bf.knnMatch(des1, des2, k=2)
+    return sum_of_squared_diff
+
+def two_best_matcher(des_left, des_right):
+    matches = []
+    progress = 0
+    one_percent = len(des_left)/100
+    for i in range(len(des_left)): # for each query descriptor
+        index_best = -1
+        index_second_best = -1
+        best_dist = float('inf')
+        second_best_dist = float('inf')
+        for j in range(len(des_right)): # check against all training descriptors
+            # calculate ssd
+            ssd = calculate_ssd(des_left[i], des_right[j])
+            # if ssd is less than best dist
+            if ssd < best_dist:
+                # update second best dist and index
+                second_best_dist = best_dist
+                index_second_best = index_best
+
+                # update best dist and index
+                best_dist = ssd
+                index_best = j
+            # if ssd is greater than best but less than second best
+            elif ssd > best_dist and ssd < second_best_dist:
+                # update second best dist and index
+                second_best_dist = ssd
+                index_second_best = j
+        progress = progress + 1
+        if progress % one_percent == 0:
+            print(f"{progress/one_percent} percent complete")
+        # create best match object
+        match_best = cv2.DMatch(int(i), int(index_best), float(best_dist))
+        # create second best match object
+        match_second_best = cv2.DMatch(int(i), int(index_second_best), float(second_best_dist))
+        # add both objects to a tuple
+        match_tuple = (match_best, match_second_best)
+        # append tuple to matches
+        matches.append(match_tuple)
+
+    return matches
+
+def draw_feature_matching(img_left, img_right):
+    # Find keypoints and descriptors useing the sift_detector function
+    img_left, kp_left, des_left = sift_detector(img_left, False, True)
+    img_right, kp_right, des_right = sift_detector(img_right, False, True)
+
+    # find matches using left as query descriptors and right as training descriptors
+    matches = two_best_matcher(des_left, des_right)
 
     # Apply ratio test
     good = []
-    for m, n in matches:
-        if m.distance < 0.75 * n.distance:
-            good.append([m])
+    Threshold = 0.7
+    for m in matches:
+        if m[0].distance / m[1].distance < Threshold:
+            good.append([m[0]])
 
     # cv.drawMatchesKnn expects list of lists as matches.
-    img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, good, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+    img3 = cv2.drawMatchesKnn(img_left, kp_left, img_right, kp_right, good, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 
-    img3 = resize(img3, 1400, 500)
     return img3
 
-def stitch(img_, img):
-    img1 = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
-    img2 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+def stitch(img, img_):
 
-    sift = cv2.SIFT_create()
-    # find the keypoints and descriptors with SIFT
-    kp1, des1 = sift.detectAndCompute(img1, None)
-    kp2, des2 = sift.detectAndCompute(img2, None)
-
+    kp1, des1 = sift_detector(img_, False, False)
+    kp2, des2 = sift_detector(img, False, False)
+    print(kp1.shape)
+    print(des1.shape)
+    print(kp2.shape)
+    print(des2.shape)
     # BFMatcher with default params
     bf = cv2.BFMatcher()
-    matches = bf.knnMatch(des1, des2, k=2)
+    matches = two_best_matcher(des2, des1)
 
     # Apply ratio test
     good = []
@@ -121,14 +156,20 @@ def stitch(img_, img):
 
     dst = cv2.warpPerspective(img_, H, (img.shape[1] + img_.shape[1], img.shape[0]))
     dst[0:img.shape[0], 0:img.shape[1]] = img
-    dst = resize(dst, 1400, 500)
     return dst
 
+def setup_frames(filename1, filename2):
+    frame1 = cv2.imread(filename1)
+    frame2 = cv2.imread(filename2)
+    frame1 = resize(frame1, 700, 500)
+    frame2 = resize(frame2, 700, 500)
+    return frame1, frame2
 
 def gui():
     sg.theme("LightGreen")
 
     # Define the window layout
+
 
     layout = [
 
@@ -138,10 +179,10 @@ def gui():
 
         [
             sg.Text("Image File 1"),
-            sg.In(size=(25, 1), enable_events=True, key="-FILE-"),
+            sg.In(size=(40, 1), enable_events=True, key="-FILE-"),
             sg.FileBrowse(),
             sg.Text("Image File 2"),
-            sg.In(size=(25, 1), enable_events=True, key="-FILE2-"),
+            sg.In(size=(40, 1), enable_events=True, key="-FILE2-"),
             sg.FileBrowse(),
         ],
 
@@ -154,12 +195,9 @@ def gui():
 
         [
             sg.Radio("Harris Corner Detector", "Radio", size=(20, 1), key="-HARRIS CORNER-"),
-            sg.Radio("Harris Corner Detector (dilated)", "Radio", size=(30, 1), key="-HARRIS CORNER DILATED-")
-        ],
-
-        [
+            sg.Radio("Harris Corner Detector (dilated)", "Radio", size=(30, 1), key="-HARRIS CORNER DILATED-"),
             sg.Radio("SIFT feature point Detector", "Radio", size=(20, 1), key="-SIFT-"),
-            sg.Radio("Display flags", "Radio", size=(10, 1), key="-FLAG-"),
+            sg.Radio("SIFT feature point Detector (Display flags)", "Radio", size=(40, 1), key="-FLAG-"),
         ],
 
         [sg.Radio("Brute Force Matcher", "Radio", size=(20, 1), key="-MATCHER-")],
@@ -169,7 +207,6 @@ def gui():
         [sg.Button("Exit", size=(10, 1))],
 
     ]
-
     # Create the window and show it without the plot
 
     window = sg.Window("OpenCV Integration", layout, location=(800, 400))
@@ -178,6 +215,7 @@ def gui():
     show_both = False
     filename1 = ""
     filename2 = ""
+    prev = ""
 
     while True:
 
@@ -188,71 +226,84 @@ def gui():
 
         if event == "-FILE-":
             filename1 = values ["-FILE-"]
+            prev = ""
         elif event == "-FILE2-":
             filename2 = values["-FILE2-"]
+            prev = ""
 
         if filename1 != "" and filename2 != "":
-            frame1 = cv2.imread(filename1)
-            frame2 = cv2.imread(filename2)
 
             if values["-NORMAL-"]:
                 show_both = True
 
-                frame1 = cv2.imread(filename1)
-                frame1 = resize(frame1, 700, 500)
-                frame2 = cv2.imread(filename2)
-                frame2 = resize(frame2, 700, 500)
-
-                just_switched = True
+                if prev != "normal":
+                    frame1, frame2 = setup_frames(filename1, filename2)
+                    just_switched = True
+                    prev = "normal"
 
             elif values["-HARRIS CORNER-"]:
                 show_both = True
 
-                frame1 = harris_corner(frame1)
-                frame2 = harris_corner(frame2)
-
-                just_switched = True
+                if prev != "harris corner":
+                    frame1, frame2 = setup_frames(filename1, filename2)
+                    frame1 = harris_corner(frame1, False)
+                    frame2 = harris_corner(frame2, False)
+                    just_switched = True
+                    prev = "harris corner"
 
             elif values["-HARRIS CORNER DILATED-"]:
                 show_both = True
 
-                frame1 = harris_corner_dilated(frame1)
-                frame2 = harris_corner_dilated(frame2)
-
-                just_switched = True
+                if prev != "harris corner dilated":
+                    frame1, frame2 = setup_frames(filename1, filename2)
+                    frame1 = harris_corner(frame1, True)
+                    frame2 = harris_corner(frame2, True)
+                    just_switched = True
+                    prev = "harris corner dilated"
 
             elif values["-SIFT-"]:
                 show_both = True
 
-                frame1, kp, des = sift_detector(frame1, False)
-                frame2, kp, des = sift_detector(frame2, False)
-
-                just_switched = True
+                if prev != "sift":
+                    frame1, frame2 = setup_frames(filename1, filename2)
+                    frame1, kp, des = sift_detector(frame1, False, True)
+                    frame2, kp, des = sift_detector(frame2, False, True)
+                    just_switched = True
+                    prev = "sift"
 
             elif values["-FLAG-"]:
                 show_both = True
 
-                frame1, kp, des = sift_detector(frame1, True)
-                frame2, kp, des = sift_detector(frame2, True)
-
-                just_switched = True
+                if prev != "flag":
+                    frame1, frame2 = setup_frames(filename1, filename2)
+                    frame1, kp, des = sift_detector(frame1, True, True)
+                    frame2, kp, des = sift_detector(frame2, True, True)
+                    just_switched = True
+                    prev = "flag"
 
             elif values["-MATCHER-"]:
                 show_both = False
 
-                frame1 = brute_force_matching(frame1, frame2)
-
-                just_switched = True
+                if prev != "matcher":
+                    frame1, frame2 = setup_frames(filename1, filename2)
+                    frame1 = draw_feature_matching(frame1, frame2)
+                    just_switched = True
+                    prev = "matcher"
 
             elif values["-STITCH-"]:
                 show_both = False
 
-                frame1 = stitch(frame2, frame1)
-
-                just_switched = True
+                if prev != "stitch":
+                    frame1, frame2 = setup_frames(filename1, filename2)
+                    frame1 = stitch(frame1, frame2)
+                    just_switched = True
+                    prev = "stitch"
 
         if just_switched and show_both:
-
+            # if radio just switched and both frames should be shown,
+            # resize frames and update them, make frame 2 visible
+            frame1 = resize(frame1, 700, 500)
+            frame2 = resize(frame2, 700, 500)
             imgbytes = cv2.imencode(".png", frame1)[1].tobytes()
             imgbytes2 = cv2.imencode(".png", frame2)[1].tobytes()
 
@@ -262,7 +313,9 @@ def gui():
             just_switched = False
 
         elif just_switched and not show_both:
-
+            # if radio just switched and only frame 1 should be shown,
+            # resize frame 1 and update it, make frame 2 not visible
+            frame1 = resize(frame1, 1400, 500)
             imgbytes = cv2.imencode(".png", frame1)[1].tobytes()
 
             window["-IMAGE-"].update(data=imgbytes)
@@ -276,6 +329,6 @@ if __name__ == '__main__':
     # harris_corner_detector("wall.png")
     # sift_detector("wall.png")
     gui()
-    #img = cv2.imread("Images/left.png")
-    #img2 = cv2.imread("Images/right.png")
-    #stitch(img2, img)
+    #left = cv2.imread("Images/left.png")
+    #right = cv2.imread("Images/right.png")
+    #brute_force_matching(left, right)
